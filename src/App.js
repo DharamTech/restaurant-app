4a57771599aedfec6ee54defd4959f8fb7868c84
DharamTech <dharminderkumar0475@gmail.com>
2020-09-05 15:42:08 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import Home from "./components/Home";
+import RestaurantUpdate from "./components/RestaurantUpdate";
+import RestaurantCreate from "./components/RestaurantCreate";
+import RestaurantDetail from "./components/RestaurantDetail";
+import RestaurantSearch from "./components/RestaurantSearch";
+import RestaurantList from "./components/RestaurantList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Router>
+            <ul>
+                       <li><Link to="/">Home</Link></li>
+                          <li><Link to="/list">List</Link></li>
+                              <li><Link to="/create">Create</Link></li>
+                                <li><Link to="/search">Search</Link></li>
+                            <li><Link to="/detail">Details</Link></li>
+                   <li> <Link to="/update">Update</Link></li>
+             </ul>
+        
+         </Router>
     </div>
   );
 }
